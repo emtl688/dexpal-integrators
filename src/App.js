9a673acaeb6c268c00871033932d5442bec36143
costa2400 <costag2400@gmail.com>
2025-07-20 16:22:17 -0400

    Initial commit - DexPal Dashboard implementation

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DexPalPartnershipDashboard from './components/DexPalPartnershipDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <DexPalPartnershipDashboard />
+    </div>
+  );
+}
+
+export default App; 
